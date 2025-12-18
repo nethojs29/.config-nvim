7349c3df28ab057330a7fa2f54e900f06d267d02
Ernesto Salazar <nethojs29@gmail.com>
2025-12-17 23:32:30 -0700

    yank highlight

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,21 @@ vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.winborder = "rounded"
+vim.api.nvim_set_option("clipboard","unnamed")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch", -- or "Visual"
+      timeout = 200,
+    })
+  end,
+})
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
