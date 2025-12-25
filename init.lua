9bdbebc113a92349ef4ddef9ed241f76ae9ab63a
Ernesto Salazar <nethojs29@gmail.com>
2025-12-24 17:38:40 -0700

    whichkey before config

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-require('config.options')
 require('config.keybinds')
 require('config.lazy')
 require('config.lsp')
+require('config.options')
--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,16 +1,3 @@
 vim.g.mapleader = " "
 
-local wk = require("which-key")
-wk.add({
-  { "<leader>a", group = "Gemini" }, -- group
-  { "<leader>f", group = "Telescope" }, -- group
-  { "<leader>h", group = "Harpoon" }, -- group
-},
-  {
-    -- Nested mappings are allowed and can be added in any order
-    -- Most attributes can be inherited or overridden on any level
-    -- There's no limit to the depth of nesting
-    mode = { "n", "v" }, -- NORMAL and VISUAL mode
-    { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
-    { "<leader>w", "<cmd>w<cr>", desc = "Write" },
-  })
+
